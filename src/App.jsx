08d31495bb6cce740adba15ca6d7f76f6615d10a
Abdullahi Khalif <abdullajust2021@gmail.com>
2023-08-27 17:15:33 +0300

    adding Timer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './Components/Header/Header'
+import Timer from './Components/Timer/Timer'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
      <div>
       <Header/>
+      <Timer/>
      </div>
     </>
   )
